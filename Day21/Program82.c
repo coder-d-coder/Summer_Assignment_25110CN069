de36e4aff4903cd05c35bd86e52e2c380d4e2a7c
Sarthak Chaudhary <sarthakpatelkd@gmail.com>
2026-07-02 23:56:02 +0530

    Day 21 Assignment

---
+++ Day21/Program82.c
@@ -0,0 +1,32 @@
+// Program to reverse a string without using strrev() function
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100], temp;
+    int i, length;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    length = strlen(str);
+
+    // Remove newline character if present
+    if (str[length - 1] == '\n')
+    {
+        str[length - 1] = '\0';
+        length--;
+    }
+
+    // Reverse the string
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
