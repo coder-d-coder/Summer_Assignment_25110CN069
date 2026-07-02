64e2dc8ccfe3d5f879ca40806018328945a116b8
Sarthak Chaudhary <sarthakpatelkd@gmail.com>
2026-07-02 23:54:48 +0530

    Day 18 Assignment

---
+++ Day18/Program69.c
@@ -0,0 +1,55 @@
+// Program to impolement Bubble sort
+#include <stdio.h>
+void bubblesort(int arr[], int n)
+{
+    int i, j, temp, swapped;
+    for (i = 0; i < n - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (swapped == 0)
+            break;
+    }
+}
+
+void display(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of the array :");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array :\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array before sorting:\n");
+    display(arr, n);
+
+    bubblesort(arr, n);
+
+    printf("Array after Sorting :\n");
+    display(arr, n);
+
+    return 0;
+}
